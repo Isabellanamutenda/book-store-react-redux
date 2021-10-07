af622b05fc381354e9bd923da4589ab75257db34
Isabellanamutenda <namtendaisabella.com>
2021-10-07 12:28:04 +0300

    Add code to the index.js file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import store from './redux/configureStore';
+import './App.css';
+import BooksContainer from './components/Books/BooksContainer';
+import Categories from './components/Categories/Categories';
+import Navbar from './components/Main/Navbar';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Provider store={store}>
+            <BooksContainer />
+          </Provider>
+        </Route>
+        <Route path="/categories">
+          <Categories />
+        </Route>
+      </Switch>
     </Router>
   </React.StrictMode>,
   document.getElementById('root'),
