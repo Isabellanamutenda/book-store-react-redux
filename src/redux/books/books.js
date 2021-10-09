318e510e2f900fd4710a46723edf6f2acf069d52
Isabellanamutenda <namtendaisabella.com>
2021-10-09 19:25:01 +0300

    Make changes

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,58 +1,86 @@
-import axios from 'axios';
-
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
-const GET_BOOKS = 'bookStore/books/GET_BOOKS';
-const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/8uyccLaygYAG9c3K3wyv/books';
+const FETCH_DATA = 'bookStore/books/FETCH_DATA';
+const SEND_DATA = 'bookStore/books/SEND_DATA';
 const initialState = [];
 
-export const removeBook = (payload) => async (dispatch) => {
-  const { data } = await axios.delete(
-    `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/8uyccLaygYAG9c3K3wyv/books/${payload}`,
+export const addBook = (payload) => ({
+  type: ADD_BOOK,
+  payload,
+});
+
+export const fetchData = () => async (dispatch) => {
+  const response = await fetch(
+    'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/8uyccLaygYAG9c3K3wyv/books',
   );
-  if (data) {
-    dispatch({ type: REMOVE_BOOK, payload });
-  }
+
+  const json = await response.json();
+  const entries = Object.entries(json);
+
+  const books = entries.map((element) => ({
+    item_id: element[0],
+    title: Object.assign(...element[1]).title,
+    category: Object.assign(...element[1]).category,
+  }));
+
+  dispatch({
+    type: FETCH_DATA,
+    payload: books,
+  });
 };
 
-export const addBook = (payload) => async (dispatch) => {
-  const { data } = await axios.post(
-    url,
-    payload,
+export const sendData = (book) => async (dispatch) => {
+  fetch(
+    'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/8uyccLaygYAG9c3K3wyv/books',
     {
+      method: 'POST',
+      body: JSON.stringify({
+        item_id: book.item_id,
+        title: book.title,
+        category: book.category,
+      }),
       headers: {
-        'Content-Type': 'application/json;charset=UTF-8',
+        'Content-type': 'application/json; charset=UTF-8',
       },
     },
   );
-  if (data) {
-    dispatch({ type: ADD_BOOK, payload });
-  }
-};
-
-export const getBooks = () => async (dispatch) => {
-  const { data } = await axios.get(url);
-  const formattedBooks = [];
-  Object.keys(data).forEach((key) => {
-    if (key) {
-      formattedBooks.push({ ...data[key][0], item_id: key });
-    }
+  dispatch({
+    type: SEND_DATA,
+    payload: book,
   });
-  dispatch({ type: GET_BOOKS, payload: formattedBooks });
 };
 
-const reducer = (state = initialState, action) => {
-  const { type, payload } = action;
-  switch (type) {
-    case ADD_BOOK:
-      return [...state, payload];
+export const removeBook = (book) => async (dispatch) => {
+  fetch(
+    `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/8uyccLaygYAG9c3K3wyv/books/${book.item_id}`,
+    {
+      method: 'DELETE',
+    },
+  );
+  dispatch({
+    type: REMOVE_BOOK,
+    payload: book,
+  });
+};
+
+const booksReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_DATA:
+      return action.payload;
+    case SEND_DATA:
+      return [
+        ...state,
+        {
+          item_id: action.payload.item_id,
+          title: action.payload.title,
+          category: action.payload.category,
+        },
+      ];
     case REMOVE_BOOK:
-      return state.filter((book) => book.item_id !== payload);
-    case GET_BOOKS:
-      return payload;
+      return state.filter((book) => book.item_id !== action.payload.item_id);
     default:
       return state;
   }
 };
 
-export default reducer;
+export default booksReducer;
